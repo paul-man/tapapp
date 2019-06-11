398084c8cc2f92c4c334d4abad9694609baa0f74
paul-man <paul.mannarino@gmail.com>
2019-06-11 15:11:06 -0400

    Update SplashActivity.java
    Adding finish() to avoid splash included in history

--- app/src/main/java/com/sackstack/tapapp/SplashActivity.java
+++ app/src/main/java/com/sackstack/tapapp/SplashActivity.java
@@ -1,22 +1,17 @@
 package com.sackstack.tapapp;
 
 import android.content.Intent;
-
 import android.os.Bundle;
-
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
-
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
         Intent intent = new Intent();
         intent.setClass(SplashActivity.this, MainActivity.class);
         startActivity(intent);
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+        finish();
     }
 }
